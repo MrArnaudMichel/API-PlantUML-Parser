400bd6a2bc6c65ed639c0ebf19e2a99ea9719eae
Arnaud Michel <arnaudd.michel@gmail.com>
2023-05-10 15:46:44 +0200

    Semaine 2 terminé - Rajout de l'option --help et --DCA. - Génération du DCA demandé. - Création d'un début de DCC (enlever --DCA de la commande) - Division du code en beaucoup de classe. - DCC API

--- src/pumlFromJava/classes/Classe.java
+++ src/pumlFromJava/classes/Classe.java
@@ -2,61 +2,6 @@ package pumlFromJava.classes;
 
 import java.util.ArrayList;
 
-public class Classe implements Object {
-    private String name;
-    private ArrayList<Attributs> attributes = new ArrayList<Attributs>();
-    private ArrayList<Methode> methods = new ArrayList<Methode>();
-    private ArrayList<String> extendsClasses = new ArrayList<String>();
-    private ArrayList<String> implementsInterfaces = new ArrayList<String>();
-    private String namePackage;
+public class Classe extends Object {
 
-    public Classe(){}
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ArrayList<Attributs> getAttributes() {
-        return attributes;
-    }
-
-    public void setAttributes(ArrayList<Attributs> attributes) {
-        this.attributes = attributes;
-    }
-
-    public ArrayList<Methode> getMethods() {
-        return methods;
-    }
-
-    public void setMethods(ArrayList<Methode> methods) {
-        this.methods = methods;
-    }
-
-    public ArrayList<String> getExtendsClasses() {
-        return extendsClasses;
-    }
-
-    public void setExtendsClasses(ArrayList<String> extendsClasses) {
-        this.extendsClasses = extendsClasses;
-    }
-
-    public ArrayList<String> getImplementsInterfaces() {
-        return implementsInterfaces;
-    }
-
-    public void setImplementsInterfaces(ArrayList<String> implementsInterfaces) {
-        this.implementsInterfaces = implementsInterfaces;
-    }
-
-    public String getNamePackage() {
-        return namePackage;
-    }
-
-    public void setNamePackage(String namePackage) {
-        this.namePackage = namePackage;
-    }
 }
--- src/pumlFromJava/classes/Object.java
+++ src/pumlFromJava/classes/Object.java
@@ -1,15 +1,65 @@
 package pumlFromJava.classes;
 
+import pumlFromJava.classes.Attributs;
+import pumlFromJava.classes.Methode;
+
 import java.util.ArrayList;
 
-public interface Object {
-    String getName();
-    void setName(String name) ;
+public class Object {
+    private String name;
+    private ArrayList<Attributs> attributes = new ArrayList<Attributs>();
+    private ArrayList<Methode> methods = new ArrayList<Methode>();
+    private ArrayList<String> extendsClasses = new ArrayList<String>();
+    private ArrayList<String> implementsInterfaces = new ArrayList<String>();
+    private String namePackage;
 
-    ArrayList<Attributs> getAttributes();
+    public Object(){}
 
-    void setAttributes(ArrayList<Attributs> attributes);
-    ArrayList<Methode> getMethods();
+    public String getName() {
+        return name;
+    }
 
-    void setMethods(ArrayList<Methode> methods);
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Attributs> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(ArrayList<Attributs> attributes) {
+        this.attributes = attributes;
+    }
+
+    public ArrayList<Methode> getMethods() {
+        return methods;
+    }
+
+    public void setMethods(ArrayList<Methode> methods) {
+        this.methods = methods;
+    }
+
+    public ArrayList<String> getExtendsClasses() {
+        return extendsClasses;
+    }
+
+    public void setExtendsClasses(ArrayList<String> extendsClasses) {
+        this.extendsClasses = extendsClasses;
+    }
+
+    public ArrayList<String> getImplementsInterfaces() {
+        return implementsInterfaces;
+    }
+
+    public void setImplementsInterfaces(ArrayList<String> implementsInterfaces) {
+        this.implementsInterfaces = implementsInterfaces;
+    }
+
+    public String getNamePackage() {
+        return namePackage;
+    }
+
+    public void setNamePackage(String namePackage) {
+        this.namePackage = namePackage;
+    }
 }
